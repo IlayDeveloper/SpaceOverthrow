ef139b596d430217df6d524b365c38bfa1976089
tsyman <mr.tsyman@mail.ru>
2020-03-24 17:24:55 +0400

    add gravity for multi targets

--- Assets/Scripts/Gravitation.cs
+++ Assets/Scripts/Gravitation.cs
@@ -1,29 +1,37 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Gravitation : MonoBehaviour
 {
-    public float g = 9.8f;
+    public const float g = 9.8f;
     private float planetMass;
-    private float shipMass;
-    public Rigidbody rigidbody;
-    //private 
-    // Start is called before the first frame update
+    //private float shipMass;
+    //public Rigidbody rigidbody;
+    public static List<Rigidbody> targets { get; set; }
     void Start()
     {
-        planetMass = rigidbody.mass;
-        shipMass = SpaceShip.Instance.rigidbody.mass;
-
+        planetMass = GetComponent<Rigidbody>().mass;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (SpaceShip.Instance != null)
+        /*if (SpaceShip.Instance != null)
         {
             float force = g * (planetMass * shipMass) / Vector3.SqrMagnitude(SpaceShip.Instance.transform.position - transform.position);
             SpaceShip.Instance.rigidbody.AddForce(( transform.position - SpaceShip.Instance.transform.position ).normalized * force);
+        }*/
+
+        foreach (var target in targets)
+        {
+            AddForce(target);
         }
+    }
+
+    private void AddForce(Rigidbody target)
+    {
+        float force = g * (planetMass * target.mass) / Vector3.SqrMagnitude(SpaceShip.Instance.transform.position - transform.position);
+        target.AddForce(( transform.position - target.transform.position ).normalized * force);
     }
 }
---
+++ Assets/Scripts/GravityTarget.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class GravityTarget : MonoBehaviour
+{
+    public Rigidbody rigidBody { get; private set; }
+
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void OnEnable()
+    {
+        if(Gravitation.targets == null)
+            Gravitation.targets = new List<Rigidbody>();
+        
+        Gravitation.targets.Add(rigidBody);
+    }
+
+    private void OnDisable()
+    {
+        Gravitation.targets.Remove(rigidBody);
+    }
+}
