d9ed5fa3940666d7dacbaae85eba176e6ca57ba8
mr.tsyman@mail.ru <github45698027ru>
2020-04-10 17:48:59 +0400

    Menu scene and MainMenuController.cs

---
+++ Assets/Scripts/Gameplay/MainMenuController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void ToGameScene()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void QuitApp()
+    {
+        Application.Quit();
+    }
+}
