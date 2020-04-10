2e21625a7bbbca7cefae350c85274d5f9c209e42
mr.tsyman@mail.ru <github45698027ru>
2020-04-10 15:35:56 +0400

    Camera view to isometric

--- Assets/Scripts/Follower.cs
+++ Assets/Scripts/Follower.cs
@@ -3,13 +3,21 @@
 public class Follower : MonoBehaviour
 {
     public SpaceShip spaceShip;
-    public Transform camera;
+    public Camera camera;
     public float down;
     public float up;
+    private Vector3 offset;
 
-    void Start()
+    void Awake()
     {
-        camera.position = spaceShip.transform.position + spaceShip.transform.up * down;
+        // camera.position = spaceShip.transform.position + spaceShip.transform.up * down;
+        offset = camera.transform.position - spaceShip.transform.position;
+    }
+    
+    private void OnValidate()
+    {
+        // camera.position = spaceShip.transform.position + spaceShip.transform.up * down;
+        camera.orthographicSize = down;
     }
 
     // Update is called once per frame
@@ -18,6 +26,8 @@ public class Follower : MonoBehaviour
         if(!spaceShip) return;
         float speed = spaceShip.rigidbody.velocity.magnitude;
         float maxSpeed = spaceShip.maxSpeed;
-        camera.position = spaceShip.transform.position + spaceShip.transform.up * Mathf.Lerp(down, up, speed / maxSpeed);
+        // camera.position = spaceShip.transform.position + spaceShip.transform.up * Mathf.Lerp(down, up, speed / maxSpeed);
+        camera.orthographicSize =  Mathf.Lerp(down, up, speed / maxSpeed);;
+        camera.transform.position = spaceShip.transform.position + offset;
     }
 }
